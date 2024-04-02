8004789557b43e85f84a3a4b8f702506289578b0
zubi120 <za1225370@gmail.com>
2024-04-03 01:25:30 +0500

    hm
    hm

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 // 1) computer will generate a random number
 // 2)user input for guessing number
 // 3) compare user input number and computer generated number then shown result
-const randomNumber = 1999;
+const randomNumber = Math.floor(Math.random() * 10 + 1);
 const answer = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "please guess a number",
+        message: "please guess a number between 1 to 6",
     },
 ]);
 if (answer.userGuessedNumber === randomNumber) {
